cc2bd7c650b79e11c23ded1825daebd3bcb6dde9
aman <aman313@gmail.com>
2024-05-03 16:24:51 +0530

    first commit

---
+++ functions/functions.py
@@ -0,0 +1,9 @@
+
+def return_42():
+    return 42
+
+def return_x(x):
+    return x
+
+def return_x_plus_42(x)
+    return return_x(x) + return 42
---
+++ main/main.py
@@ -0,0 +1,3 @@
+from functions import return_x_plus_42
+
+return_x_plus_42(10)_
